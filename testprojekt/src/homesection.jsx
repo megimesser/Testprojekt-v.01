9ba643c2de886e79568a7858d1d3a444ac2852ab
megimesser <heinze.obach@web.de>
2024-08-19 15:39:14 +0200

    neue homepage

---
+++ testprojekt/src/homesection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home section of your application.</p>
+    </div>
+  );
+}
+
+export default Home;
